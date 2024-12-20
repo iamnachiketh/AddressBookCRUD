483fd7e156fea0e090b29f53704eff0e1fe250ee
Nachiketha <nachiketh.k.c@brainsightai.com>
2024-12-20 19:52:52 +0530

    display welcome to adress book

--- AddressBookMain.js
+++ AddressBookMain.js
@@ -1,4 +1,28 @@
 
-class AddressMainBook{
-    
+const fs = require('fs');
+
+class AddressMainBook {
+    constructor(firstName, lastName, address, city, state, zip, phone, email) {
+
+        this.firstName = firstName;
+
+        this.lastName = lastName;
+
+        this.address = address;
+
+        this.city = city;
+
+        this.state = state;
+
+        this.zip = zip;
+
+        this.phone = phone;
+
+        this.email = email;
+    }
+
+
+    displayWelcome() {
+        console.log("Welcome to Address Book");
+    }
 }
