3def6b58ce77b3e9a63b06a6ffad0bdb5ef791ca
Nachiketha <nachiketh.k.c@brainsightai.com>
2024-12-20 19:57:18 +0530

    created the details of the person

--- AddressBookMain.js
+++ AddressBookMain.js
@@ -25,4 +25,22 @@ class AddressMainBook {
     displayWelcome() {
         console.log("Welcome to Address Book");
     }
+
+    createDetails(){
+        
+        let data = `First Name: ${this.firstName}\nLast Name: ${this.lastName}\nAddress: ${this.address}\nCity: ${this.city}\nState: ${this.state}\nZip: ${this.zip}\nPhone: ${this.phone}\nEmail: ${this.email}\n\n\n`;
+
+        fs.appendFile('addressBook.txt', data,(error)=>{
+            if(error){
+                console.log("Error in writing file");
+            }else{
+                console.log("Data written successfully");
+            }
+        });
+    }
+
 }
+
+let addressBook1 = new AddressMainBook("Rahul", "Kumar", "Kankarbagh", "Patna", "Bihar", 800020, 1234567890, "kethan@yahoo.com");
+
+addressBook1.createDetails();
